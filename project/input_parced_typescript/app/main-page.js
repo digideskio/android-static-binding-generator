027406bfde31ca7fc49de620173414c9e32a224d
plamen5kov <plamen.krasimirov.petkov@gmail.com>
2016-02-24 16:42:57 +0200

    removed unnecessary test files

--- project/input_parced_typescript/app/main-page.js
+++
@@ -1,8 +0,0 @@
-var vmModule = require("./main-view-model");
-var app = require("application")
-var context = app.android.context;
-function pageLoaded(args) {
-    var page = args.object;
-    page.bindingContext = vmModule.mainViewModel;
-}
-exports.pageLoaded = pageLoaded;
