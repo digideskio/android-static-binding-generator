77a0c58ca1b498508a662f3f02912aac0b0e62d5
Peter Kanev <pip3r4o@gmail.com>
2016-06-20 16:06:10 +0300

    add js_parser traversing tests

--- tests/specs/ast-parser-tests.spec.js
+++ tests/specs/ast-parser-tests.spec.js
@@ -6,14 +6,46 @@ var exec = require("child_process").exec,
     parser = path.resolve(__dirname, "../../project/ast-parser/js_parser.js");
 
 describe("ast-parser/js_parser tests", function () {
-    xdescribe("js_parser tests", function () {
-        xit("", function () {
+    describe("js_parser tests", function () {
+        it("Analyse files only in the correct folder structure", function (done) {
+            var input = prefix + "/mini_app",
+                output = prefix + "/mini_app/bindings.txt";
 
+            exec("node " + parser + " " + input + " " + output + " " + interfaceNames, function (err) {
+                var bindingsContent = fs.readFileSync(output, "utf-8").toString().trim();
+
+                var expectedExtends = [
+                    "a.B.C.ButtonThatWillAlwaysBeExtended",
+                    "a.B.C.ButtonThatIsExtendedComponent",
+                    "a.B.C.ButtonThatIsExtendedComponent.Subcomponent",
+                    "a.B.C.ButtonThatIsExtendedComponent1",
+                    "a.B.C.ButtonThatIsExtendedComponent2",
+                    "a.B.C.ButtonThatIsExtendedComponent2.Subcomponent21",
+                    
+                ];
+                var notExpectedExtends = [
+                    "a.B.C.ButtonThatShouldNotBeExtended",
+                    "a.B.C.ButtonThatShouldNotBeExtended.Submodule",
+                    "a.B.C.ButtonThatShouldNotBeExtendedComponent.Subcomponent"
+                ];    
+
+                for(var entry in expectedExtends) {
+                    var expectedValue = expectedExtends[entry] + "*";
+                    expect(bindingsContent).toContain(expectedValue);
+                }
+
+                for(var entry in notExpectedExtends) {
+                    var notExpectedValue = notExpectedExtends[entry] + "*";
+                    expect(bindingsContent).toNotContain(notExpectedExtends);
+                }
+
+                done();
+            });
         })
     });
 
     describe("es5-visitors tests", function () {
-        it("Generated metadata for bindings should match the pattern JavaClass*extendPrefix*methods*NewClass*Location ---> /([a-zA-Z_$][a-zA-Z\d_$]*\.)*[a-zA-Z_$][a-zA-Z\d_$]*\*([\_A-z0-9]*)\*([\_A-z][A-z0-9]*)*(\,[\_A-z][A-z0-9]*)*\*(([a-zA-Z_$][a-zA-Z\d_$]*\.)*[a-zA-Z_$][a-zA-Z\d_$]*)*\*((?:[a-zA-Z]\:){0,1}(?:[\\/]*[\w.]+){1,})*/", function (done) {
+        it("Generated metadata for bindings should match the pattern JavaClass*extendPrefixLoc*methods*NewClass*Location ---> /([a-zA-Z_$][a-zA-Z\d_$]*\.)*[a-zA-Z_$][a-zA-Z\d_$]*\*([\_A-z0-9]*)\*([\_A-z][A-z0-9]*)*(\,[\_A-z][A-z0-9]*)*\*(([a-zA-Z_$][a-zA-Z\d_$]*\.)*[a-zA-Z_$][a-zA-Z\d_$]*)*\*((?:[a-zA-Z]\:){0,1}(?:[\\/]*[\w.]+){1,})*/", function (done) {
             var pattern = /(([a-zA-Z_$][a-zA-Z\d_$]*\.)*[a-zA-Z_$][a-zA-Z\d_$]*\*([\_A-z0-9]*)\*([\_A-z][A-z0-9]*)*(\,[\_A-z][A-z0-9]*)*\*(([a-zA-Z_$][a-zA-Z\d_$]*\.)*[a-zA-Z_$][a-zA-Z\d_$]*)*\*((?:[a-zA-Z]\:){0,1}(?:[\\/]*[\w.]+){1,})*)+/mi;
 
             var input = prefix + "/normal_ns_extends",
