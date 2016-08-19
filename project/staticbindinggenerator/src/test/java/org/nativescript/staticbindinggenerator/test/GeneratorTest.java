803ddc1b6d879aa128981e61cb1dacf55c80aa60
Peter Kanev <pip3r4o@gmail.com>
2016-08-19 14:02:45 +0300

    add tests

--- project/staticbindinggenerator/src/test/java/org/nativescript/staticbindinggenerator/test/GeneratorTest.java
+++ project/staticbindinggenerator/src/test/java/org/nativescript/staticbindinggenerator/test/GeneratorTest.java
@@ -1,7 +1,9 @@
 package org.nativescript.staticbindinggenerator.test;
 
+import com.example.Guest;
 import com.example.MyInterface;
 
+import org.apache.bcel.classfile.Method;
 import org.apache.commons.io.IOUtils;
 import org.junit.Assert;
 import org.junit.BeforeClass;
@@ -59,25 +61,22 @@ public class GeneratorTest {
 
     @Test
     public void testCanCompileBindingOfInterfaceWithStaticInitializer() throws Exception {
-        URL u = MyInterface.class.getResource('/' + MyInterface.class.getName().replace('.', '/') + ".class");
-        File f = new File(u.toURI()).getParentFile().getParentFile().getParentFile();
 
         File rt = new File(System.getProperty("java.home"), "lib/rt.jar");
         Assert.assertTrue(rt.exists());
 
-        String s = IOUtils.toString(this.getClass().getResource("datarow-classctor.txt"), "UTF-8");
-        List<String> lines = IOUtils.readLines(new StringReader(s));
+        List<String> lines = Utils.getDataRowsFromResource("datarow-classctor.txt");
         DataRow dataRow = new DataRow(lines.get(0));
 
         String outputDir = null;
-        String[] libs = {rt.getAbsolutePath(), f.getAbsolutePath()};
+        String[] libs = {rt.getAbsolutePath(), dependenciesDir};
         Generator generator = new Generator(outputDir, libs);
         Binding binding = generator.generateBinding(dataRow);
 
         StringBuffer sourceCode = new StringBuffer();
         sourceCode.append(binding.getContent());
 
-        Iterable<String> options = new ArrayList<String>(Arrays.asList("-cp", f.getAbsolutePath()));
+        Iterable<String> options = new ArrayList<String>(Arrays.asList("-cp", dependenciesDir));
         Class<?> helloClass = InMemoryJavaCompiler.compile("com.tns.gen.com.example.MyInterface", sourceCode.toString(), options);
 
         Assert.assertNotNull(helloClass);
@@ -102,5 +101,66 @@ public class GeneratorTest {
 
         Assert.assertNotNull(ComplexClass);
         Assert.assertEquals(5, ComplexClass.getInterfaces().length); // 4 + 1 (hashcodeprovider)
+    }
+
+    @Test
+    public void testCanCompileBindingClassImplementingMultipleInterfacesWithTheSameSignature() throws Exception {
+        List<String> lines = Utils.getDataRowsFromResource("datarow-class-extends-interfaces-with-same-method.txt");
+        DataRow dataRow = new DataRow(lines.get(0));
+
+        String outputDir = "generated";
+        String[] libs = {runtimePath, dependenciesDir};
+        Generator generator = new Generator(outputDir, libs);
+        Binding binding = generator.generateBinding(dataRow);
+
+        StringBuffer sourceCode = new StringBuffer();
+        sourceCode.append(binding.getContent());
+
+        Iterable<String> options = new ArrayList<String>(Arrays.asList("-cp", dependenciesDir));
+        Class<?> MyClassWithPresent = InMemoryJavaCompiler.compile(binding.getClassname(), sourceCode.toString(), options);
+
+        Assert.assertNotNull(MyClassWithPresent);
+        Assert.assertEquals(3, MyClassWithPresent.getInterfaces().length); // 2 + 1 (hashcodeprovider)
+
+        java.lang.reflect.Method[] methods = MyClassWithPresent.getMethods();
+        boolean foundPresent = false;
+        for (java.lang.reflect.Method method : methods) {
+            if(method.getName() == "present") {
+                if(foundPresent) {
+                    Assert.fail("Only 1 void method 'present' should be written.");
+                } else {
+                    foundPresent = true;
+                }
+            }
+        }
+
+        Assert.assertTrue(foundPresent);
+    }
+
+    @Test
+    public void testWillThrowWhenTryingToCompileBindingClassImplementingMultipleInterfacesWithTheMethodNameButDifferentReturnType() throws Exception {
+        List<String> lines = Utils.getDataRowsFromResource("datarow-extends-interfaces-with-similar-methods-different-return.txt");
+        DataRow dataRow = new DataRow(lines.get(0));
+
+        String outputDir = "generated";
+        String[] libs = {runtimePath, dependenciesDir};
+        Generator generator = new Generator(outputDir, libs);
+        Binding binding = generator.generateBinding(dataRow);
+
+        StringBuffer sourceCode = new StringBuffer();
+        sourceCode.append(binding.getContent());
+
+        Iterable<String> options = new ArrayList<String>(Arrays.asList("-cp", dependenciesDir));
+        Class<?> MyClassWithPresent = null;
+        boolean caughtException = false;
+
+        try {
+            MyClassWithPresent = InMemoryJavaCompiler.compile(binding.getClassname(), sourceCode.toString(), options);
+        } catch (java.lang.ClassFormatError exception) {
+            caughtException = true;
+        }
+
+        Assert.assertTrue(caughtException);
+        Assert.assertNull(MyClassWithPresent);
     }
 }
